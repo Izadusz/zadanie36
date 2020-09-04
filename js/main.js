08a93d9246a6caeb859ee789374550e8b0cd1524
Iza Duszyńska <iza.duszynska@wp.pl>
2020-09-04 14:30:14 +0200

    zadanie36

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function displayResults(num1, num2) {
+    let add = num1 + num2;
+    if (num1 === 20) {
+        console.log("True");
+    } 
+    if (num2 === 20) {
+        console.log("True");
+    } 
+    if (add <= 20) {
+        console.log("True");
+    }
+}
+displayResults(2, 20);
