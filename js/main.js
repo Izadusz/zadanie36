f6bb5383e93570607125ef648913f08d28bcc126
Iza Duszyńska <iza.duszynska@wp.pl>
2020-09-08 15:59:34 +0200

    poprawa

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function displayResults(num1, num2) {
     let add = num1 + num2;
     if (num1 === 20) {
-        console.log("True");
-    } 
+        return ("True");
+    }
     if (num2 === 20) {
-        console.log("True");
-    } 
+        return ("True");
+    }
     if (add <= 20) {
-        console.log("True");
+        return ("True");
     }
 }
 displayResults(2, 20);
